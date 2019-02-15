1ab07ae835dc2736ca9413d57c954b6c7622de50
Eduardo Martinez <marti540@couagrs.csusm.edu>
2019-02-14 21:55:26 -0800

    intital

---
+++ Homework3_2/musicPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Homework3_2
+{
+    public partial class musicPage : ContentPage
+    {
+        public musicPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
